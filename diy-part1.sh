31ba1b59f8b275630548049b59c61adbf002cb77
zhoudedi <64512474+zhoudedi@users.noreply.github.com>
2022-02-11 22:38:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+git clone https://github.com/kenzok8/small-package package/small-package
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
